f6f8d15b8b502883a888bda4e7250d91de21db67
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-09-27 17:57:08 +0200

    adding endangered class

--- src/main/java/Endangered.java
+++ src/main/java/Endangered.java
@@ -1,13 +1,13 @@
-//import java.util.List;
-//import org.sql2o.*;
-//public class Endangered extends Animal{
-//    private String health;
-//    private String age;
-//    public Endangered( String name, String health, String age) {
-//        this.name = name;
-//        this.health = health;
-//        this.age = age;
-//    }
+import java.util.List;
+import org.sql2o.*;
+public class Endangered extends Animal{
+    private String health;
+    private String age;
+    public Endangered( String name, String health, String age) {
+        this.name = name;
+        this.health = health;
+        this.age = age;
+    }
 //
 //    public String getHealth(){
 //        return health;
