060f1bd4bb7f76d95dace3ab65a4916fd18e3e74
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-09-27 18:11:23 +0200

    adding sighting class

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,12 +1,12 @@
-//
-//
-//public class Sighting extends Animal {
-//    private String location;
-//    private String ranger;
-//    public Sighting(String location, String ranger){
-//        this.location = location;
-//        this.ranger = ranger;
-//    }
+
+
+public class Sighting extends Animal {
+    private String location;
+    private String ranger;
+    public Sighting(String location, String ranger){
+        this.location = location;
+        this.ranger = ranger;
+    }
 //    public String getLocation() {
 //        return location;
 //    }
