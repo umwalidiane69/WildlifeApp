6dfc932f500cee9bd591125e43c0222aefd3599e
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-09-27 18:17:30 +0200

    adding code to animal2 class

--- src/main/java/Animal2.java
+++ src/main/java/Animal2.java
@@ -2,7 +2,7 @@ public class Animal2 extends Animal {
     public Animal2(String name){
         this.name = name;
     }
-//    public String getName() {
-//        return name;
-//    }
-//}
+    public String getName() {
+        return name;
+    }
+}
