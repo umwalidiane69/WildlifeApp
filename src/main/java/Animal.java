ead1c72cb9c3e8d5d80aacada1df4c35e8a86d8e
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-09-27 18:20:54 +0200

    adding code for animal class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -5,13 +5,13 @@ public abstract class Animal {
     public String name;
     public int id;
     public void save() {
-//        try (Connection con = DB.sql2o.open()) {
-//            String sql = "INSERT INTO animal (name) VALUES (:name,:id)";
-//            this.id = (int) con.createQuery(sql, true)
-//                    .addParameter("name", this.name)
-////                    .addParameter("id", this.id)
-//                    .executeUpdate()
-//                    .getKey();
-//        }
-//    }
-//}
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animal (name) VALUES (:name,:id)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+//                    .addParameter("id", this.id)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+}
