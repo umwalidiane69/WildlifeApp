6799a218d20bc3929dae42b61d29abd9d51cb8fd
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-09-27 18:24:23 +0200

    get animal-form and post them

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,26 +5,26 @@ import static spark.Spark.*;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 
-//public class App {
-//    public static void main(String[] args){
-//        staticFileLocation("/public");
-//        get("/", (request, response) -> {
-//            Map<String, Object>  model= new HashMap();
-//            return new ModelAndView (model,"Animal_form.hbs");
-//        }, new HandlebarsTemplateEngine());
-//
-//        post("mamals/new", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            String name = request.queryParams("name");
-//            String health = request.queryParams("health");
-//            String age = request.queryParams("age");
-//            Endangered animal= new Endangered(name,health,age);
-//            model.put("animal", animal);
-//            model.put("name", name);
-//            model.put("health", health);
-//            model.put("age", age);
-//            return new ModelAndView(model, "Allanimals.hbs");
-//        }, new HandlebarsTemplateEngine());
+public class App {
+    public static void main(String[] args){
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object>  model= new HashMap();
+            return new ModelAndView (model,"Animal_form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("mamals/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String health = request.queryParams("health");
+            String age = request.queryParams("age");
+            Endangered animal= new Endangered(name,health,age);
+            model.put("animal", animal);
+            model.put("name", name);
+            model.put("health", health);
+            model.put("age", age);
+            return new ModelAndView(model, "Allanimals.hbs");
+        }, new HandlebarsTemplateEngine());
 //
 //        get("/mamal/new", (request, response) -> {
 //            Map<String, Object>  model= new HashMap();
