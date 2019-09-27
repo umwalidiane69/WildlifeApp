8d99dfad474af66f1ebfdd82aa543e5ad9a17645
Agnes Nyiramisigaro <aggyreina@gmail.com>
2019-09-27 18:19:13 +0200

    adding animal class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,10 +1,10 @@
-//import java.util.ArrayList;
-//import java.util.List;
-//import org.sql2o.*;
-//public abstract class Animal {
-//    public String name;
-//    public int id;
-//    public void save() {
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+public abstract class Animal {
+    public String name;
+    public int id;
+    public void save() {
 //        try (Connection con = DB.sql2o.open()) {
 //            String sql = "INSERT INTO animal (name) VALUES (:name,:id)";
 //            this.id = (int) con.createQuery(sql, true)
